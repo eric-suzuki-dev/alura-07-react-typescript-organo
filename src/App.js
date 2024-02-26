555266f424504a22bf50e1c02ff9341151a64441
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-02-25 22:52:42 -0300

    Modulo 01.aula 06

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { useState } from 'react';
-import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 import Time from './componentes/Time';
+
+import Banner from './componentes/Banner'
 
 function App() {
 
@@ -52,16 +53,16 @@ function App() {
 
   return (
     <div className="App">
-      <Banner />
-      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      <Banner enderecoImagem='/imagens/banner.png' />
+      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
 
-      {times.map(time => <Time 
-        key={time.nome} 
-        nome={time.nome} 
-        corPrimaria={time.corPrimaria} 
-        corSecundaria={time.corSecundaria} 
+      {times.map(time => <Time
+        key={time.nome}
+        nome={time.nome}
+        corPrimaria={time.corPrimaria}
+        corSecundaria={time.corSecundaria}
         colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
-      />)}   
+      />)}
 
     </div>
   );
